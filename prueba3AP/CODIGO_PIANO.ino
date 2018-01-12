25079aae728ea5511e5e20c6d07e3a2c48823034
cesaraul <cesaraulcg@gmail.com>
2018-01-12 12:52:49 -0500

    Código arduino_piano
    consigo con delay de 100ms, para garantizar la reproducción del sonido

---
+++ prueba3AP/CODIGO_PIANO.ino
@@ -0,0 +1,43 @@
+/*
+  AnalogReadSerial
+  Reads an analog input on pin 0, prints the result to the serial monitor.
+  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+*/
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  int sensorValue1 = analogRead(A1);
+  int sensorValue2 = analogRead(A2); 
+  // print out the value you read:
+  if (sensorValue>700){
+    Serial.println("C");
+    delay(100);        // delay in between reads for stability
+  }
+   
+  if (sensorValue1>700){
+    Serial.println("D");
+    delay(100);        // delay in between reads for stability
+  }
+   
+  if (sensorValue2>700){
+    Serial.println("E");
+    delay(100);        // delay in between reads for stability
+  }
+//  else{
+//    Serial.print("Z");
+//    delay(100);
+//    }
+
+
+}
