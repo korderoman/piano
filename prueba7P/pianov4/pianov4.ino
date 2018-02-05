b82e8e2ef342a0516489922dc13d2450b25c55a2
Henry <elpadredelcordero@gmail.com>
2018-02-05 15:59:49 -0500

    Prueba Arduino
    Prueba arduino piano

---
+++ prueba7P/pianov4/pianov4.ino
@@ -0,0 +1,25 @@
+int valorSensor1;
+int valorSensor2;
+int sensores[6];
+String valores[6]={"c","d","g","e","f","a"};
+
+void setup() {
+ Serial.begin(9600);      
+  
+}
+
+void loop() {
+  for(int i=0;i<6;i++){
+      sensores[i]=analogRead('A'+i);   
+    }
+
+  for(int i=0;i<6;i++){
+      if(sensores[i]>500){
+          Serial.println(valores[i]);
+          delay(100);         
+        }
+    
+    }
+
+
+}
